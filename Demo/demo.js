65fa87ad23566aa7397a8815b40f974392ac1b7c
Francis Perea <francis@francisperea.org>
2012-06-25 23:47:02 +0200

    Simplifico cancelación drag

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,27 +1,26 @@
 $(function() {
 
-$('#outer').draggable({
-    start: function(e, ui) {
-        if ($(e.originalEvent.target).is(".scroll")) {
-            e.stopPropagation();
-            e.preventDefault();
+    $('#outer').draggable({
+        start: function(e, ui) {
+            if ($(e.originalEvent.target).is(".scroll")) {
+                e.preventDefault();
+            }
         }
-    }
-});
+    });
 
 
-$('#nested').click(function(event) {
-    event.stopPropagation();
-    $('#outer').effect('scale', {
-        scale: 'content',
-        percent: 50
-    }, 1000);
+    $('#nested').click(function(event) {
+        event.stopPropagation();
+        $('#outer').effect('scale', {
+            scale: 'content',
+            percent: 50
+        }, 1000);
 
-});
-$('#outer').click(function() {
-    $('#outer').effect('scale', {
-        scale: 'content',
-        percent: 200
-    }, 1000);
-});
+    });
+    $('#outer').click(function() {
+        $('#outer').effect('scale', {
+            scale: 'content',
+            percent: 200
+        }, 1000);
+    });
 });​
